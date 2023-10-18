c77015a0a14a546688c89321b5952d846b3b4d97
zainabmd <izainabmahmood@gmail.com>
2023-10-18 21:51:47 +0500

    set up firebase for project

---
+++ index.js
@@ -0,0 +1,10 @@
+const appSetting = {
+    databaseURL:"https://playground-bffce-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const inputF= document.getElementById("input-field");
+const addBtn=document.getElementById("add-btn");
+
+addBtn.addEventListener("click", function(){
+    
+})
