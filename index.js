74ee9cec1151e78d46062ffdb6af85c29ff9742e
zainabmd <izainabmahmood@gmail.com>
2023-11-06 23:34:43 +0500

    abandoning this module project because i dont like his way of teaching

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push,onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 const appSetting = {
     databaseURL:"https://playground-bffce-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
@@ -15,7 +15,29 @@ const shoppingList=document.getElementById("shopping-list")
 addBtn.addEventListener("click", function(){
     let inputValue=inputF.value;
     push(shoppingListinDB , inputValue);
-    inputF.value="";
-    shoppingList.innerHTML+=`<li>${inputValue}</li>`
-    console.log(inputValue);
+    clearInputField();
 })
+onValue(shoppingListinDB,function(snapshot){
+    let itemsArray=Object.entries(snapshot.val())
+    clearShoppingList();
+    for(let i=0;i<itemsArray.length;i++){
+        let currentItem=itemsArray[i];
+        let currentID=currentItem[0]
+        let currentValue=currentItem[1]
+        appendToShoppingList(currentValue);
+    }
+})
+function clearInputField(){
+    inputF.value="";
+}
+function clearShoppingList(){
+    shoppingList.value="";
+}
+function appendToShoppingList(itemValue){
+    //shoppingList.innerHTML+=`<li>${itemValue}</li>`
+    let itemID=item[0];
+    let itemValue=item[1]
+    let newEl = document.createElement("li")
+    newEl.textContent = itemValue
+    shoppingList.append(newEl)
+}
